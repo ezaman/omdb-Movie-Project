74481bcd1d304ea963a5e568d9a4e6f3517ab60b
ezaman <ehsanmzaman@gmail.com>
2016-09-07 13:20:39 -0400

    initial commit

---
+++ omdb-movie-project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  omdb-movie-project
+//
+//  Created by Ehsan Zaman on 9/7/16.
+//  Copyright © 2016 Ehsan Zaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //initial commit
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
